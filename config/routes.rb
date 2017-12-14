b62ab18b50450115b48c4af58cc447eaeafb2a59
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-12-14 00:35:30 +0000

    partial API controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get '/regu', to: 'pages#regu', as: :regu
-  devise_for :users
-root to: 'pages#index'
-resources :registeredapps
-end
+
+
+    get '/regu', to: 'pages#regu', as: :regu
+    devise_for :users
+    root to: 'pages#index'
+    resources :registeredapps
+
+
+    namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+    end
+end 
