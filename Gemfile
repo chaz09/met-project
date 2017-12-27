8e0f81388d6801bd91b77e248dbc279762205230
Charlyn Oyakhilome <charlynoyakhilome@gmail.com>
2017-12-27 15:10:00 +0000

    client side javascript done

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ source 'https://rubygems.org'
    gem 'web-console', '~> 2.0'
  end
 
+gem 'bootstrap-sass', '~> 3.3.7'
  gem 'sass-rails', '~> 5.0'
  gem 'uglifier', '>= 1.3.0'
  gem 'coffee-rails', '~> 4.1.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
 //= require Chart.bundle
 //= require chartkick
 <%= javascript_include_tag "https://www.gstatic.com/charts/loader.js" %>
-var blocmetrics = {};
-  blocmetrics.report = function(eventName){
-    var event = {event: { name: eventName }};
-
-    var request = new XMLHttpRequest();
-
-    request.open("POST", "http://localhost:3000/api/events", true);
-
-    request.setRequestHeader('Content-Type', 'application/json');
-
-    request.send(JSON.stringify(event));
-  };
